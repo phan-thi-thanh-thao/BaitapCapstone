bd7bf7d2562024ab7e92f9a02b3c549affaaf03d
Thao <thao81010@gmail.com>
2025-03-15 02:11:27 +0700

    upadte vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {
       fontFamily: {
         'sans': ['Poppins', 'sans-serif'],
-        'serif': ['Merriweather', 'serif'],
+        'serif': ['Merriweather', 'serif']
       },
       colors: {
         'primary': '#3858F6',
         'secondary': '#F6F8FC',
         'dark': '#1D2630',
         'theme-light': '#F6F8FC',
-        'border': '#E1E9F8',
-        'gradient': 'linear-gradient(90deg, #ED3251 0%, #F6635D 100%)'
-      },
+        'border': '#E1E9F8'
+      }
     },
     container: {
       center: true,
       padding: '1rem'
     }
   },
-  plugins: [],
+  plugins: []
 }
