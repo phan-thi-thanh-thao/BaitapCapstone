40486d740dcdf44e5de9f9eb2d0cffdabcf0d51f
Thao <thao81010@gmail.com>
2025-03-16 16:44:32 +0700

    update code 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,28 @@
 module.exports = {
   content: [
-    "./public/**/*.{html,js}",
-    "./src/**/*.{html,js}"
+    "./*.{html,js,jsx,ts,tsx}", // Chỉ quét thư mục `src`
+    "./public/index.html"
   ],
   theme: {
     extend: {
+
       fontFamily: {
         'sans': ['Poppins', 'sans-serif'],
-        'serif': ['Merriweather', 'serif']
+        'serif': ['Merriweather', 'serif'],
       },
       colors: {
         'primary': '#3858F6',
         'secondary': '#F6F8FC',
         'dark': '#1D2630',
         'theme-light': '#F6F8FC',
-        'border': '#E1E9F8'
-      }
+        'border': '#E1E9F8',
+        'gradient': 'linear-gradient(90deg, #ED3251 0%, #F6635D 100%)'
+      },
     },
     container: {
       center: true,
       padding: '1rem'
     }
   },
-  plugins: []
+  plugins: [],
 }
